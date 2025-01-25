0c0e1bd75ab5e1caeb6ce73df7814ca97f6219e8
Anis Belkacem <belkac01@ads.uni-passau.de>
2025-01-25 13:25:38 +0100

    20 commit

--- src/de/uni_passau/fim/se2/sbse/suite_generation/algorithms/RandomSearch.java
+++ src/de/uni_passau/fim/se2/sbse/suite_generation/algorithms/RandomSearch.java
@@ -61,16 +61,17 @@ public class RandomSearch<C extends Chromosome<C>> implements GeneticAlgorithm<C
                         ? Double.MAX_VALUE 
                         : bestFitnessFunction.applyAsDouble(bestCandidate);
                     stoppingCondition.notifyFitnessEvaluation();
-                    if (bestCandidate == null || distance < bestDistance) {
+
+                    if (distance < bestDistance) {
                         bestSolutions.put(branch, candidate);
                         
                     }
                 }
             }
             uncoveredBranches.removeAll(branchesToRemove);
-            if(bestSolutions.size() % populationSize == 0){
+            /*if(bestSolutions.size() % populationSize == 0){
                 stoppingCondition.notifyIteration();
-            }
+            }*/
             
         }
         return new ArrayList<>(bestSolutions.values());
