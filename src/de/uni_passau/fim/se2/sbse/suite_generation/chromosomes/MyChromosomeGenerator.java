1cdc0662ae76751b563f54eb7a6f7fe191c64534
Anis Belkacem <belkac01@ads.uni-passau.de>
2025-01-26 22:43:06 +0100

    99

--- src/de/uni_passau/fim/se2/sbse/suite_generation/chromosomes/MyChromosomeGenerator.java
+++ src/de/uni_passau/fim/se2/sbse/suite_generation/chromosomes/MyChromosomeGenerator.java
@@ -45,7 +45,7 @@ public class MyChromosomeGenerator implements ChromosomeGenerator<MyChromosome> 
     @Override
     public MyChromosome get() {
         //int numberOfStat = random.random().nextInt(30) + 30; 
-        int numberOfStat =60; 
+        int numberOfStat =55; 
         List<Statement> statements = new ArrayList<>();
         Object instance = Instance(statements);
         if (instance != null) { 
